bee4159cd2526483d20d0532644b8498bb807a74
Tysuiku <Timothydong41@gmail.com>
2022-11-07 16:19:36 -0500

    working

--- algorithmic_strategies_exercise/01_swapper.rb
+++ algorithmic_strategies_exercise/01_swapper.rb
@@ -2,12 +2,12 @@
 # The method should swap the elements at the given indices and return the array.
 # The method should modify the existing array and not create a new array.
 
+def swapper(arr, x, y)
+    arr[x] , arr[y] = arr[y], arr[x]
+    arr
+end  
 
 p swapper(["a", "b", "c", "d"], 0, 2)               # => ["c", "b", "a", "d"]
 p swapper(["a", "b", "c", "d"], 3, 1)               # => ["a", "d", "c", "b"]
 p swapper(["canal", "broadway", "madison"], 1, 0)   # => ["broadway", "canal", "madison"]
 
-def swapper(arr,*x, y)
-    arr[x] , arr[y] = arr[x], arr[y]
-    return arr
-end
