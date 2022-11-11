ce60c5505f2ef606b466368374e1e10b336972b0
Tysuiku <Timothydong41@gmail.com>
2022-11-11 17:26:09 -0500

    work

--- App_AcademyWork/Projects_Week1/Day5/Meddling_Medley/meddling_medley.rb
+++ App_AcademyWork/Projects_Week1/Day5/Meddling_Medley/meddling_medley.rb
@@ -2,8 +2,8 @@
 
 def duos(str)
     count = 0
-        (0...str.length).each do |i|
-            if str[i] = str[i+1] 
+        (0...str.length - 1).each do |i|
+            if str[i] == str[i+1] 
                 count += 1
             end
         end
@@ -11,8 +11,25 @@ def duos(str)
     count 
 end
 
-p duos('bootcamp')      # 1
-p duos('wxxyzz')        # 2
-p duos('hoooraay')      # 3
-p duos('dinosaurs')     # 0
-p duos('e')             # 0
+# p duos('bootcamp')      # 1
+# p duos('wxxyzz')        # 2
+# p duos('hoooraay')      # 3
+# p duos('dinosaurs')     # 0
+# p duos('e')             # 0
+
+#sentence_swap
+def sentence_swap()
+
+end
+
+p sentence_swap('anything you can do I can do',
+    'anything'=>'nothing', 'do'=>'drink', 'can'=>'shall'
+) # 'nothing you shall drink I shall drink'
+
+p sentence_swap('what a sad ad',
+    'cat'=>'dog', 'sad'=>'happy', 'what'=>'make'
+) # 'make a happy ad'
+
+p sentence_swap('keep coding okay',
+    'coding'=>'running', 'kay'=>'pen'
+) # 'keep running okay'
