d6e52a1a517fd47a570b12af8eb16680babe317b
Tysuiku <Timothydong41@gmail.com>
2022-11-11 16:56:59 -0500

    workingonmeddlingmed

---
+++ App_AcademyWork/Projects_Week1/Day5/Meddling_Medley/meddling_medley.rb
@@ -0,0 +1,18 @@
+#duos
+
+def duos(str)
+    count = 0
+        (0...str.length).each do |i|
+            if str[i] = str[i+1] 
+                count += 1
+            end
+        end
+
+    count 
+end
+
+p duos('bootcamp')      # 1
+p duos('wxxyzz')        # 2
+p duos('hoooraay')      # 3
+p duos('dinosaurs')     # 0
+p duos('e')             # 0
