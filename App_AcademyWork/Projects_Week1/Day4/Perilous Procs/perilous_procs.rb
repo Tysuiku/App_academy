fbff609cab09c76555e7024c5f00ebd6c05a1db3
Tysuiku <Timothydong41@gmail.com>
2022-11-11 12:42:22 -0500

    day5

--- App_AcademyWork/Projects_Week1/Day4/Perilous Procs/perilous_procs.rb
+++ App_AcademyWork/Projects_Week1/Day4/Perilous Procs/perilous_procs.rb
@@ -143,3 +143,14 @@ end
 # p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
 # p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }            # nil
 
+#xnor_select
+def xnor_select
+
+end
+
+is_even = Proc.new { |n| n % 2 == 0 }
+is_odd = Proc.new { |n| n % 2 != 0 }
+is_positive = Proc.new { |n| n > 0 }
+p xnor_select([8, 3, -4, -5], is_even, is_positive)         # [8, -5]
+p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive)  # [-7, -13, 12]
+p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)   # [5, -10]
