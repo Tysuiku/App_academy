67d093976cd53dce5cd0c6c31624fc3d34b93604
Tysuiku <Timothydong41@gmail.com>
2022-11-11 16:42:44 -0500

    rspecexcercise5

--- App_AcademyWork/Projects_Week1/Day5/rspec_exercise_5/lib/exercise.rb
+++ App_AcademyWork/Projects_Week1/Day5/rspec_exercise_5/lib/exercise.rb
@@ -1,12 +1,9 @@
-def zip(*array) 
-    length = array.first.length
-    newarr = []
-    (0...length).each do |i|
-    array.each do |arr|
-            newarr << arr[i]
+def zip(*arrays)
+    length = arrays.first.length
+
+    (0...length).map do |i|
+        arrays.map { |array| array[i] }
     end
-    end
-    newarr
 end
 
 def prizz_proc(arr, prc1, prc2)
@@ -20,10 +17,70 @@ def prizz_proc(arr, prc1, prc2)
     newarr
 end
 
-def zany_zip(*array)
+def zany_zip(*arrays)
+    length = arrays.map(&:length).max
 
+    (0...length).map do |i|
+        arrays.map { |array| array[i] }
+    end
 end
 
-def maximum()
+def maximum(array, &prc)
+    return nil if array.empty?
+    max = array.first
+    array.each do |el|
+        max = el if prc.call(el) >= prc.call(max)
+    end
+    max
+end
 
+def my_group_by(array, &prc)
+    groups = Hash.new { |h, k| h[k] = [] }
+    array.each do |el|
+        key = prc.call(el)
+        groups[key] << el
+    end
+    groups
+end
+
+def max_tie_breaker(array, tie_breaker, &prc)
+    return nil if array.empty?
+    max = array.first
+    array.each do |el|
+        result_el = prc.call(el)
+        result_max = prc.call(max)
+        if result_el > result_max
+            max = el
+        elsif result_el == result_max && tie_breaker.call(el) > tie_breaker.call(max)
+            max = el
+        end
+    end
+    max
+end
+
+def change_word(word)
+    indices = vowel_indices(word)
+    word[indices.first..indices.last]
+end
+
+def vowel_indices(word)
+    vowels = 'aeiou'
+    indices = []
+    word.each_char.with_index do |char, i|
+        indices << i if vowels.include?(char)
+    end
+    indices
+end
+
+def silly_syllables(sentence)
+    words = sentence.split(' ')
+    new_words = words.map do |word|
+        num_vowels = vowel_indices(word).length
+        if num_vowels < 2
+            word
+        else
+            change_word(word)
+        end
+    end
+    new_words.join(' ')
 end
