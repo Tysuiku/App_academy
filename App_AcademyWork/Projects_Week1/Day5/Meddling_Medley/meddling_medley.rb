fc426172d1b5bb8428093d23ee347bb64bc18858
Tysuiku <Timothydong41@gmail.com>
2022-11-11 21:00:34 -0500

    workedmoreonMeddling

--- App_AcademyWork/Projects_Week1/Day5/Meddling_Medley/meddling_medley.rb
+++ App_AcademyWork/Projects_Week1/Day5/Meddling_Medley/meddling_medley.rb
@@ -18,18 +18,75 @@ end
 # p duos('e')             # 0
 
 #sentence_swap
-def sentence_swap()
+def sentence_swap(sentence, hash)
+    newsen = []
+    sentence.split(" ").each do |word|
+        if hash[word] != nil
+            newsen << hash[word]
+        else 
+            newsen << word
+        end
+    end
 
+    newsen.join(" ")
 end
 
-p sentence_swap('anything you can do I can do',
-    'anything'=>'nothing', 'do'=>'drink', 'can'=>'shall'
-) # 'nothing you shall drink I shall drink'
+# p sentence_swap('anything you can do I can do',
+#     'anything'=>'nothing', 'do'=>'drink', 'can'=>'shall'
+# ) # 'nothing you shall drink I shall drink'
 
-p sentence_swap('what a sad ad',
-    'cat'=>'dog', 'sad'=>'happy', 'what'=>'make'
-) # 'make a happy ad'
+# p sentence_swap('what a sad ad',
+#     'cat'=>'dog', 'sad'=>'happy', 'what'=>'make'
+# ) # 'make a happy ad'
 
-p sentence_swap('keep coding okay',
-    'coding'=>'running', 'kay'=>'pen'
-) # 'keep running okay'
+# p sentence_swap('keep coding okay',
+#     'coding'=>'running', 'kay'=>'pen'
+# ) # 'keep running okay'
+
+#hash_mapped
+def hash_mapped(hash, prc, &block)
+    newhash = {}
+    hash.each do |k, v|
+        newhash[block.call(k)] = prc.call(v)
+    end
+    newhash
+end
+
+# double = Proc.new { |n| n * 2 }
+# p hash_mapped({'a'=>4, 'x'=>7, 'c'=>-3}, double) { |k| k.upcase + '!!' }
+# # {"A!!"=>8, "X!!"=>14, "C!!"=>-6}
+
+# first = Proc.new { |a| a[0] }
+# p hash_mapped({-5=>['q', 'r', 's'], 6=>['w', 'x']}, first) { |n| n * n }
+# # {25=>"q", 36=>"w"}
+
+#counted_characters
+def char_count(str)
+    hash = {}
+    str.each_char do |char|
+        if hash[char] == nil
+            hash[char] = 1
+        else 
+            hash[char] += 1
+        end
+    end
+
+    hash
+end
+
+def counted_characters(str)
+    hash = char_count(str)
+    arr = []
+        str.each_char do |char|
+            if hash[char] > 2
+                arr << char
+            end
+        end
+    
+    arr
+end
+
+p counted_characters("that's alright folks") # ["t"]
+p counted_characters("mississippi") # ["i", "s"]
+p counted_characters("hot potato soup please") # ["o", "t", " ", "p"]
+p counted_characters("runtime") # []
